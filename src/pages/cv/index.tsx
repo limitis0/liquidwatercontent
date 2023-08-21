022348819f71669c32ddfed141c1eab1874e33e1
leetw131 <leetw@jgallop.com>
2023-08-21 15:37:42 +0800

    - PDF Viewer (have not tested yet) - add sample file

--- src/pages/cv/index.tsx
+++ src/pages/cv/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Document, pdfjs } from "react-pdf";
+import classes from "./cv.module.scss";
 
 interface CVProps {}
 
 const CV: React.FC<CVProps> = () => {
-  return <div>CV</div>;
+  // worker
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "pdfjs-dist/build/pdf.worker.min.js",
+    import.meta.url
+  ).toString();
+
+  return (
+    <div className={classes.pdf_viewer}>
+      <div style={{ width: "70%" }}>
+        <Document file="https://github.com/limitis0/liquidwatercontent/blob/main/public/sample_pdf.pdf" />
+      </div>
+    </div>
+  );
 };
 
 export default CV;
